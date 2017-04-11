2928425a4c539644c2185cf222c1812fe372a62b
Joel Meador <joel@expectedbehavior.com>
2017-04-11 14:43:02 -0400

    modern honeybadger has gotten rid of notify_or_ignore

--- lib/instrumental/reporters/honeybadger.rb
+++ lib/instrumental/reporters/honeybadger.rb
@@ -16,13 +16,6 @@ module Instrumental
           end
           alias_method_chain :notify, :instrumental
 
-          def notify_or_ignore_with_instrumental(exception, options = {})
-            notify_or_ignore_without_instrumental(exception, options)
-            InstrumentalReporters.agent.increment("exception")
-            InstrumentalReporters.agent.increment("exception.#{InstrumentalReporters.dotify(exception.class.name.underscore)}") if exception
-          end
-          alias_method_chain :notify_or_ignore, :instrumental
-
         end
       end
     end
