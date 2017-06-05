05503c75cc9113263e1dbdb6feec2e1601d82bae
Joel Meador <joel@expectedbehavior.com>
2017-06-05 16:59:21 -0400

    rubocop whitespace

--- lib/resque/resque_plugin.rb
+++ lib/resque/resque_plugin.rb
@@ -1,4 +1,5 @@
 module Instrumental
+
   module ResquePlugin
     def around_perform_with_instrumental(*args)
       start = Time.now
@@ -11,13 +12,17 @@ module Instrumental
     def on_failure_with_instrumental(e, *args)
       InstrumentalReporters.agent.increment("#{Instrumental::ResqueHelper.job_metric_instrumentation_name(self)}.error")
     end
+
   end
 
   module ResqueHelper
+
     def self.job_metric_instrumentation_name(job)
       # remove all #, ?, !, etc. as well as runs of . and ending .'s
       name = job.to_s.gsub(/[^\w]+/, ".").gsub(/\.+$/, "")
       "jobs.#{name}"
     end
+
   end
+
 end
