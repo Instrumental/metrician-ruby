6983f7ab490456ecd6a3077379417295e48071c5
Joel Meador <joel@expectedbehavior.com>
2017-06-05 14:47:08 -0400

    use the gauge proxy for Net::HTTP reporting

--- lib/instrumental/reporters/net_http.rb
+++ lib/instrumental/reporters/net_http.rb
@@ -6,22 +6,18 @@ module Instrumental
 
     def instrument
       return if ::Net::HTTP.method_defined?(:request_with_instrumental_trace)
-
       ::Net::HTTP.class_eval do
-
         def request_with_instrumental_trace(req, body = nil, &block)
           start_time = Time.now
           begin
             request_without_instrumental_trace(req, body, &block)
           ensure
-            InstrumentalReporters.agent.gauge("service.request", (Time.now - start_time).to_f)
+            InstrumentalReporters.gauge("service.request", (Time.now - start_time).to_f)
           end
         end
-
         alias_method :request_without_instrumental_trace, :request
         alias_method :request, :request_with_instrumental_trace
       end
-
     end
   end
 end
