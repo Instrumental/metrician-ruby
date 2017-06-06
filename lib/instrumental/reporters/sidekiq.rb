d60de0f58af83b919297e35febe98db70bebe997
Joel Meador <joel@expectedbehavior.com>
2017-06-06 14:11:25 -0400

    fix the sidekiq instrumentation to actually work and get loaded by reporters

--- lib/instrumental/reporters/sidekiq.rb
+++ lib/instrumental/reporters/sidekiq.rb
@@ -7,9 +7,9 @@ module Instrumental
 
     def instrument
       require "sidekiq/sidekiq_middleware"
-      Sidekiq.configure_server do |config|
+      ::Sidekiq.configure_server do |config|
         config.server_middleware do |chain|
-          chain.add Sidekiq::Middleware::Server::Logging, Instrumental::SidekiqMiddleware
+          chain.add Instrumental::SidekiqMiddleware
         end
       end
     end
--- lib/sidekiq/sidekiq_middleware.rb
+++ lib/sidekiq/sidekiq_middleware.rb
@@ -14,12 +14,12 @@ module Instrumental
       InstrumentalReporters.gauge(job_metric_instrumentation_name(worker), duration)
     end
 
-  end
+    def job_metric_instrumentation_name(worker)
+      # remove all #, ?, !, etc. as well as runs of . and ending .'s
+      name = worker.class.name.gsub(/[^\w]+/, ".").gsub(/\.+$/, "")
+      "jobs.#{name}"
+    end
 
-  def job_metric_instrumentation_name(worker)
-    # remove all #, ?, !, etc. as well as runs of . and ending .'s
-    name = worker.class.name.gsub(/[^\w]+/, ".").gsub(/\.+$/, "")
-    "jobs.#{name}"
   end
 
 end
