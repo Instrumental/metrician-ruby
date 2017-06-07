c44ae32eb3fbe605d232d3cc87d01277b72d2f10
Jason Gladish <jason@expectedbehavior.com>
2017-06-07 16:43:38 -0400

    Remove unnecessary test models
    These were copied and pasted from the gemika setup, but we only really
    need one model.

--- spec/support/database.rb
+++ spec/support/database.rb
@@ -15,18 +15,4 @@ database.rewrite_schema! do
     t.string :city
   end
 
-  create_table :recipes do |t|
-    t.string :name
-    t.integer :category_id
-  end
-
-  create_table :recipe_ingredients do |t|
-    t.string :name
-    t.integer :recipe_id
-  end
-
-  create_table :recipe_categories do |t|
-    t.string :name
-  end
-
 end
