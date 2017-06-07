162e994e9e770bc1836c5af9a9dda63d47afc4a8
Joel Meador <joel@expectedbehavior.com>
2017-06-07 09:04:11 -0400

    namespace the request timing middleware

--- lib/instrumental_reporters/railtie.rb
+++ lib/instrumental_reporters/railtie.rb
@@ -5,11 +5,9 @@ module InstrumentalReporters
       require "middleware/request_timing"
       require "middleware/application_timing"
 
-      # TODO: namespace the request and application timing bits
-      # TODO check if raw module names work in rails < 5, and split logic here if not
-      app.middleware.insert_before(0, RequestTiming)
-      app.middleware.insert_after(RequestTiming, Rack::ContentLength)
-      app.middleware.use(ApplicationTiming)
+      app.middleware.insert_before(0, Instrumental::RequestTiming)
+      app.middleware.insert_after(Instrumental::RequestTiming, Rack::ContentLength)
+      app.middleware.use(Instrumental::ApplicationTiming)
     end
 
   end
